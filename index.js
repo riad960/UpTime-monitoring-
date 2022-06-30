367c66e8ba5db7676b95a3f3b7c5772b3c227846
riad960 <riad96048@gmail.com>
2022-06-30 16:05:45 +0600

    hi

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hi");
+//dependencies
+const http = require("http");
+
+//app object - module scaffolding
+const app = {};
+
+//configuration
+app.config = {
+  port: 4000,
+};
+//create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes); //parameter e handle req res func k nebe
+  server.listen(app.config.port, () => {
+    console.log(`listening to port ${app.config.port} `);
+  });
+};
+
+//handle req res
+app.handleReqRes = (req, res) => {
+  res.end("hello world");
+};
+
+//start the server
+app.createServer();
